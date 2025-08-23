0f7f716f2111014530166a213df8ce83ed7472f6
stuffsnyc <stuffs.newyorkcity@gmail.com>
2025-08-22 20:24:05 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esta línea es crucial para que las rutas funcionen en Vercel
+  base: '', // Esta línea es CRUCIAL para Vercel
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
